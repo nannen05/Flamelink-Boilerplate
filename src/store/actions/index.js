fdce34cd8ec077d67e65d79038800379203cdfa4
nannen05 <nannen05@gmail.com>
2018-08-08 22:48:44 -0700

    removing stuff

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -3,14 +3,9 @@ import { FETCH_DATA } from "./types";
 
 export const fetchData = () => async dispatch => {
   DATAREF.on("value", snapshot => {
-  	console.log(snapshot.val())
     dispatch({
       type: FETCH_DATA,
       payload: snapshot.val()
     });
   });
 };
-
-
-
-//export default data
